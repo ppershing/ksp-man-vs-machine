5939d373058d7e952423c8d71fbd17979ea1875a
ppershing <ppershing@12315313-9e2a-11e5-be5b-b7359ef36bf2>
2011-03-17 09:09:06 +0000

    Updatnute speakovanie, rozdelene na download a play.
    git-svn-id: http://ksp-man-vs-machine.googlecode.com/svn@4 12315313-9e2a-11e5-be5b-b7359ef36bf2

--- speak.py
+++ speak.py
@@ -8,16 +8,23 @@ import os
 class MyOpener(urllib.URLopener):
   version = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11'
 
-def speak(lang, text):
+def download_speach(lang, text, filename):
+  print "Fetching speach..."
   text = re.sub(' +', ', ', text)
   baseUrl = "http://translate.google.com/translate_tts?"
   params = ({'tl' : lang, 'q' : text})
+  os.system("rm '%s'" %filename);
   stream = MyOpener().retrieve(baseUrl + urllib.urlencode(params),
-        "/tmp/speak.mp3")
-  os.system("mplayer /tmp/speak.mp3");
-  
+        filename)
+  print "Done"
 
-speak('sk', "Najvyšším vrchom Južnej Ameriky je:");
-speak('sk', "a, Ojos del Salado");
 
+def play_speach(filename):
+  os.system("mplayer '%s'" % filename)
+
+download_speach('sk', "Najvyšším vrchom Južnej Ameriky je:", "/tmp/question.mp3");
+download_speach('sk', "a, Ojos del Salado", "/tmp/answer_a.mp3");
+
+play_speach("/tmp/question.mp3");
+play_speach("/tmp/answer_a.mp3");
 
