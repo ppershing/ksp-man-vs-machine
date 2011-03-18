23c85e1d52fa32be66bd74110a09c397644d56e1
mino <mino@12315313-9e2a-11e5-be5b-b7359ef36bf2>
2011-03-18 23:07:32 +0000

    Added yes button
    git-svn-id: http://ksp-man-vs-machine.googlecode.com/svn@7 12315313-9e2a-11e5-be5b-b7359ef36bf2

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -20,8 +20,10 @@ for i in range (0, 4):
 	hbox.add (gtk.Label (name))
 	hbox.add (gtk.Entry ())
 	options += [hbox]
+
 question = gtk.Entry ()
 question.set_width_chars (52)
+name = gtk.Label ('      Steve      ')
 
 fixed.put (img, 0, 0)
 fixed.put (options[0], 120, 258)
@@ -29,6 +31,22 @@ fixed.put (options[1], 348, 258)
 fixed.put (options[2], 120, 300)
 fixed.put (options[3], 348, 300)
 fixed.put (question, 110, 190)
+
+# name
+name.set_alignment (xalign = 0, yalign = 0.5)
+#name.set_size_request (95, -1)
+#name.set_max_width_chars (15)
+#name.set_width_chars (15)
+fixed.put (name, 90, 62)
+
+#ack button
+ack = gtk.Button ()
+ack_im = gtk.Image()
+ack_im.set_from_file ('images/yes.png')
+ack.set_relief (gtk.RELIEF_NONE)
+#ack.set_size_request (100, -1)
+ack.set_image (ack_im)
+fixed.put (ack, 335, 345)
 
 w.connect('destroy', gtk.main_quit)
 w.show_all ()
