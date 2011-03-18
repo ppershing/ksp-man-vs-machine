c542a80a1361273e9bb7a6a29f8f92f37d3f6954
mino <mino@12315313-9e2a-11e5-be5b-b7359ef36bf2>
2011-03-18 23:24:10 +0000

    Correctly calls 'answers.py' script

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python
+import os
 import gtk
 import gtk.gdk
 import glib
+
+def clicked (data):
+	(stdin, stdout) = os.popen2 ('../answer.py')
+	stdin.write (question.get_text () + '\n')
+	for a in answers:
+		stdin.write (a.get_text () + '\n')
+	stdin.write ('\n')
+	stdin.flush ()
+	print stdout.readlines ()
 
 background_pixbuf = gtk.gdk.pixbuf_new_from_file('images/background.png')
 gtk.rc_parse ('mvsm.gtkrc')
@@ -13,13 +23,16 @@ fixed = gtk.Fixed ()
 w.add (fixed)
 
 options = []
+answers = []
 
 for i in range (0, 4):
 	name = '%c: ' % (65 + i,)
 	hbox = gtk.HBox ()
+	e = gtk.Entry()
 	hbox.add (gtk.Label (name))
-	hbox.add (gtk.Entry ())
+	hbox.add (e)
 	options += [hbox]
+	answers += [e]
 
 question = gtk.Entry ()
 question.set_width_chars (52)
@@ -47,6 +60,8 @@ ack.set_relief (gtk.RELIEF_NONE)
 #ack.set_size_request (100, -1)
 ack.set_image (ack_im)
 fixed.put (ack, 335, 345)
+
+ack.connect ('clicked', clicked)
 
 w.connect('destroy', gtk.main_quit)
 w.show_all ()
