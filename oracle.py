9ae265064116f9ab3e8fcb9d0c610a248b29babe
mino <mino@12315313-9e2a-11e5-be5b-b7359ef36bf2>
2011-03-19 23:32:41 +0000

    Fixed return value of unknown answer.
    git-svn-id: http://ksp-man-vs-machine.googlecode.com/svn@10 12315313-9e2a-11e5-be5b-b7359ef36bf2

--- oracle.py
+++ oracle.py
@@ -9,8 +9,9 @@ def man_vs_machine(otazka, odpovede):
 
     res = answer.vyhodnot(answer.estimate_scores(otazka_transl, odp_transl));
     if (res[0][0] < 1.0):
-        return ''
+        return None 
     return res[0][2]
-
+"""
 print man_vs_machine("Kto je autorom knihy hobit?",
     ["Dan Brown", "J.R.R tolkien", "Terry Pratchett"]);
+"""
